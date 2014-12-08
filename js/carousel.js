1c49d5bc0c586dd6ecc8bdd653234449f4cc6784
alparf <a.l.parfenkov@gmail.com>
2014-12-08 12:51:59 -0800

    update

---
+++ js/carousel.js
@@ -0,0 +1,63 @@
+var items;
+var buttons;
+var itemNumber;
+var currItem;
+
+function init() {
+	items = $(".item");
+	buttons = $(".button");
+	itemNumber = items.length;
+	currItem = 0;
+	for(var i = 0;i < itemNumber; i++) {
+		items[i].setAttribute("id", "item" + i);
+	}
+}
+
+function setItem(id) {
+	currItem = id;
+	setHeader(id);
+	$("#item" + id).fadeIn(1000);
+	buttons[id].setAttribute("class", "button active");
+	hideAll(id);
+}
+
+function nextItem() {
+	if(currItem < itemNumber-1) {
+		currItem++;
+	} else {
+		currItem = 0;
+	}
+	setHeader(currItem);
+	buttons[currItem].setAttribute("class", "button active");
+	$("#item" + currItem).fadeIn(1000);
+	hideAll(currItem);
+}
+
+function hideAll(id) {
+	for(var i = 0;i < itemNumber; i++) {
+		if(id != i) {
+			$("#item" + i).fadeOut(1000);
+			buttons[i].setAttribute("class", "button");
+		}
+	}
+}
+
+function setHeader(id) {
+	switch(id) {
+	case 0 : {
+		$("#cit").html("Имеет большой опыт в своей нише");
+		$("#cic").html("Занимаемся производством и реализацией плодоовощной продукции с 1991 года");
+		break;
+	}
+	case 1 : {
+		$("#cit").html("Высокое качество продукции");
+		$("#cic").html("Выращиваемая нами продукция проходит строгую сертификацию и отвечает высоким стандартам качестка");
+		break;
+	}
+	case 2 : {
+		$("#cit").html("Доступность");
+		$("#cic").html("Наша продукция представлена <br> в 5-ти торговых точках Гомеля");
+		break;
+	}
+	}
+}
---
+++ js/gui.js
@@ -0,0 +1,34 @@
+function menuScroll() {
+	var ws = $(window).scrollTop();
+	if(ws >= 1100) {
+		$("#aStore").addClass("now");
+		$("#aPrice").removeClass("now");
+		$("#aAbout").removeClass("now");
+	} else {
+		if(ws >= 600) {
+			$("#aPrice").addClass("now");
+			$("#aStore").removeClass("now");
+			$("#aAbout").removeClass("now");
+		} else {
+			$("#aStore").removeClass("now");
+			$("#aPrice").removeClass("now");
+			$("#aAbout").addClass("now");
+		}
+	}
+}
+
+function start(){
+	$(window).scroll(function(){
+		menuScroll();
+	});
+	$(document).ready(function(){
+	    $('a[href^="#"], a[href^="."]').click( function(){
+		    var scroll_el = $(this).attr('href');
+	        if ($(scroll_el).length != 0) { 
+		    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
+	        }
+		    return false;
+	    });
+	});
+	init();
+}
